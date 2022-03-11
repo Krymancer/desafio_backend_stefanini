5183c2ffa5c2941eab85ab8c380b6671552c1360
Júnior Nascimento <junior.nascm@gmail.com>
2022-03-11 09:59:05 -0300

    Feature: Adding City & Person to DatabaseContext

---
+++ src/Example.Infra.Data/DatabaseContext.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.Extensions.Configuration;
+
+namespace Crud.Infra.Data
+{
+    /// <summary>
+    /// Referência de artigo para conseguir criar modelos de configuração
+    /// https://docs.microsoft.com/pt-br/dotnet/architecture/microservices/microservice-ddd-cqrs-patterns/infrastructure-persistence-layer-implementation-entity-framework-core
+    /// Rererência de artigo para conseguir criar migration a partir de dominios
+    /// https://docs.microsoft.com/en-us/ef/core/managing-schemas/migrations/managing?tabs=dotnet-core-cli
+    /// </summary>
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<Domain.CityAggregate.City> City { get; set; }
+        public DbSet<Domain.PersonAggregate.Person> Person { get; set; }
+        public DatabaseContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.ApplyConfiguration(new CityEntityTypeConfiguration());
+            modelBuilder.ApplyConfiguration(new PersonEntityTypeConfiguration());
+            modelBuilder.Entity<Domain.CityAggregate.City>();
+            modelBuilder.Entity<Domain.PersonAggregate.Person>();
+
+        }
+    }
+
+    public class CityEntityTypeConfiguration : IEntityTypeConfiguration<Domain.CityAggregate.City>
+    {
+        public void Configure(EntityTypeBuilder<Domain.CityAggregate.City> orderConfiguration)
+        {
+            orderConfiguration.ToTable("City", "dbo");
+
+            orderConfiguration.HasKey(o => o.Id);
+            orderConfiguration.Property(o => o.Id).ValueGeneratedOnAdd();
+            orderConfiguration.Property(o => o.Name).IsRequired();
+            orderConfiguration.Property(o => o.UF).IsRequired();
+        }
+    }
+
+    public class PersonEntityTypeConfiguration : IEntityTypeConfiguration<Domain.PersonAggregate.Person>
+    {
+        public void Configure(EntityTypeBuilder<Domain.PersonAggregate.Person> orderConfiguration)
+        {
+            orderConfiguration.ToTable("Person", "dbo");
+
+            orderConfiguration.HasKey(o => o.Id);
+            orderConfiguration.Property(o => o.Id).ValueGeneratedOnAdd();
+            orderConfiguration.Property(o => o.Name).IsRequired();
+            orderConfiguration.Property(o => o.Age).IsRequired();
+            orderConfiguration.Property(o => o.CPF).IsRequired();
+
+            orderConfiguration.HasOne(o => o.City).WithMany(x=> x.Persons).HasForeignKey(o => o.CityId);
+        }
+    }
+}
---
+++ src/Example.Infra.Data/Migrations/20220311034230_AddBlogCreatedTimestamp.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Example.Infra.Data.Migrations
+{
+    public partial class AddBlogCreatedTimestamp : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "dbo");
+
+            migrationBuilder.CreateTable(
+                name: "City",
+                schema: "dbo",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UF = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_City", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Person",
+                schema: "dbo",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CPF = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    CityId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Person", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Person_City_CityId",
+                        column: x => x.CityId,
+                        principalSchema: "dbo",
+                        principalTable: "City",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Person_CityId",
+                schema: "dbo",
+                table: "Person",
+                column: "CityId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Person",
+                schema: "dbo");
+
+            migrationBuilder.DropTable(
+                name: "City",
+                schema: "dbo");
+        }
+    }
+}
