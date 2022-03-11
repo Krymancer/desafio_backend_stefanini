364dd2cbd9d1bb49ef40f2793d983c9e1f61ba65
Júnior Nascimento <junior.nascm@gmail.com>
2022-03-11 09:57:53 -0300

    Feature: Adding City and Person Models to Domain

---
+++ src/Example.Domain/CityAggregate/City.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crud.Domain.CityAggregate
+{
+    public class City
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string UF { get; set; }
+
+        public virtual ICollection<PersonAggregate.Person> Persons { get; set; }
+
+    }
+}
---
+++ src/Example.Domain/PersonAggregate/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crud.Domain.PersonAggregate
+{
+    public class Person
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string CPF { get; set; }
+        public int Age { get; set; }
+        public Guid CityId { get; set; }
+        virtual public CityAggregate.City City { get; set; }
+
+    }
+}
